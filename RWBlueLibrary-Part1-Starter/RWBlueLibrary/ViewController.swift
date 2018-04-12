a7f440ec9dbdac4715960dad4362ed52621860f1
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-04-12 11:53:02 -0300

    Delegate

--- RWBlueLibrary-Part1-Starter/RWBlueLibrary/ViewController.swift
+++ RWBlueLibrary-Part1-Starter/RWBlueLibrary/ViewController.swift
@@ -36,10 +36,53 @@ final class ViewController: UIViewController {
   @IBOutlet var undoBarButtonItem: UIBarButtonItem!
   @IBOutlet var trashBarButtonItem: UIBarButtonItem!
   
+  private var currentAlbumIndex = 0
+  private var currentAlbumData: [AlbumData]?
+  private var allAlbums = [Album]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    //1 Get a list of all the albums via the API. Remember, the plan is to use the facade of LibraryAPI rather than PersistencyManager directly!
+    allAlbums = LibraryAPI.shared.getAlbums()
+    
+    //2 This is where you setup the UITableView. You declare that the view controller is the UITableView data source; therefore, all the information required by UITableView will be provided by the view controller. Note that you can actually set the delegate and datasource in a storyboard, if your table view is created there.
+    tableView.dataSource = self
+    
+    showDataForAlbum(at: currentAlbumIndex)
+  }
+  
+  private func showDataForAlbum(at index: Int) {
+    
+    // defensive code: make sure the requested index is lower than the amount of albums
+    if (index < allAlbums.count && index > -1) {
+      // fetch the album
+      let album = allAlbums[index]
+      // save the albums data to present it later in the tableview
+      currentAlbumData = album.tableRepresentation
+    } else {
+      currentAlbumData = nil
+    }
+    // we have the data we need, let's refresh our tableview
+    tableView.reloadData()
   }
 
 }
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let albumData = currentAlbumData else {
+      return 0
+    }
+    return albumData.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    if let albumData = currentAlbumData {
+      let row = indexPath.row
+      cell.textLabel!.text = albumData[row].title
+      cell.detailTextLabel!.text = albumData[row].value
+    }
+    return cell
+  }
+}
